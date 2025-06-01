6a9793c5874a52b54c1ab603d8f98d52abcb35cb
Ramcharan71 <charanpanchakarla@gmail.com>
2025-06-01 22:20:59 +0530

    path changed

--- src/suby/api.js
+++ src/suby/api.js
@@ -1,3 +1,5 @@
-export const API_URL = "https://backend-nodejs-25oa.onrender.com"
+// export const API_URL = "https://backend-nodejs-25oa.onrender.com"
 
 // export const API_URL = "http://localhost:5000"
+export const API_URL = "https://backend-nodejs-1-8wgr.onrender.com"
+
